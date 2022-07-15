6c36d624bb75bbf035cc3ad92ff110d1016e2f13
epwalsh <epwalsh10@gmail.com>
2022-07-15 14:45:27 -0700

    tick version

--- steps.py
+++ steps.py
@@ -3,5 +3,8 @@ from tango import Step
 
 @Step.register("hello")
 class HelloStep(Step):
+
+    VERSION = "002"
+
     def run(self) -> None:  # type: ignore[override]
         self.logger.info("Hello!")
