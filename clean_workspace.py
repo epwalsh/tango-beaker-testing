cf79be14693bc53ee25b06926273eb20beeba9bc
epwalsh <epwalsh10@gmail.com>
2022-07-19 13:02:28 -0700

    fix

--- clean_workspace.py
+++ clean_workspace.py
@@ -1,7 +1,10 @@
 from beaker import Beaker
 
-beaker = Beaker.from_env(default_workspace="ai2/beaker-tango-testing")
-for dataset in beaker.workspace.datasets():
-    beaker.dataset.delete(dataset)
-for experiment in beaker.workspace.experiments():
-    beaker.experiment.delete(dataset)
+if __name__ == "__main__":
+    beaker = Beaker.from_env(default_workspace="ai2/beaker-tango-testing")
+    for experiment in beaker.workspace.experiments():
+        print(f"Deleting experiment {beaker.experiment.url(experiment)}")
+        beaker.experiment.delete(experiment)
+    for dataset in beaker.workspace.datasets():
+        print(f"Deleting dataset {beaker.dataset.url(dataset)}")
+        beaker.dataset.delete(dataset)
