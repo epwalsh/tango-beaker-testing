fc871236635aba8afb9b11cc4947b6b29417a654
epwalsh <epwalsh10@gmail.com>
2022-11-21 14:42:14 -0800

    clean up

--- clean_workspace.py
+++ clean_workspace.py
@@ -1,13 +1,10 @@
-from beaker import Beaker, DatasetNotFound
+from beaker import Beaker
 
 if __name__ == "__main__":
     beaker = Beaker.from_env(default_workspace="ai2/beaker-tango-testing")
+    for experiment in beaker.workspace.experiments():
+        print(f"Deleting experiment {beaker.experiment.url(experiment)}")
+        beaker.experiment.delete(experiment)
     for dataset in beaker.workspace.datasets():
         print(f"Deleting dataset {beaker.dataset.url(dataset)}")
         beaker.dataset.delete(dataset)
-    for experiment in beaker.workspace.experiments():
-        print(f"Deleting experiment {beaker.experiment.url(experiment)}")
-        try:
-            beaker.experiment.delete(experiment)
-        except DatasetNotFound:
-            pass
