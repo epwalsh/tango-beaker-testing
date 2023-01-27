77131e778b4be3ffacb60f7a6c8642404a9d5b57
epwalsh <epwalsh10@gmail.com>
2023-01-27 14:22:33 -0800

    improve clean workspace

--- clean_workspace.py
+++ clean_workspace.py
@@ -2,9 +2,4 @@ from beaker import Beaker
 
 if __name__ == "__main__":
     beaker = Beaker.from_env(default_workspace="ai2/petew-testing")
-    for experiment in beaker.workspace.experiments():
-        print(f"Deleting experiment {beaker.experiment.url(experiment)}")
-        beaker.experiment.delete(experiment)
-    for dataset in beaker.workspace.datasets():
-        print(f"Deleting dataset {beaker.dataset.url(dataset)}")
-        beaker.dataset.delete(dataset)
+    print(beaker.workspace.clear(images=False, secrets=False))
