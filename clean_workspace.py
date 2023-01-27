bcf0495df07517d006c0272ec510cb279733cf31
epwalsh <epwalsh10@gmail.com>
2023-01-27 14:18:36 -0800

    update

--- clean_workspace.py
+++ clean_workspace.py
@@ -1,7 +1,7 @@
 from beaker import Beaker
 
 if __name__ == "__main__":
-    beaker = Beaker.from_env(default_workspace="ai2/beaker-tango-testing")
+    beaker = Beaker.from_env(default_workspace="ai2/petew-testing")
     for experiment in beaker.workspace.experiments():
         print(f"Deleting experiment {beaker.experiment.url(experiment)}")
         beaker.experiment.delete(experiment)
